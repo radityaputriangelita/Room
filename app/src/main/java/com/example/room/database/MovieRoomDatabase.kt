04bf9d80baa5c967fc8d1d1eea7e2378d8838a3d
radityaputriangelita <radityaputriangelita@mail.ugm.ac.id>
2023-11-18 20:46:19 +0700

    second commit

--- app/src/main/java/com/example/room/database/MovieRoomDatabase.kt
+++ app/src/main/java/com/example/room/database/MovieRoomDatabase.kt
@@ -2,35 +2,43 @@ package com.example.room.database
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 //iniasasi entitas di Movie
-@Database(entities = [Movie::class], version = 1, exportSchema = false)
+@Database(entities = [Movie::class], version = 2, exportSchema = false)
 abstract class MovieRoomDatabase : RoomDatabase() {
-    //MovieDao untuk CRUD
+    // MovieDao untuk CRUD
     abstract fun movieDao(): MovieDao?
 
-    //database
     companion object {
         @Volatile
         private var INSTANCE: MovieRoomDatabase? = null
 
-        //function akses database
         fun getDatabase(context: Context): MovieRoomDatabase? {
-            //cek database kodong apa engga
-            //kalo datanya ga ada pake instance kalo ada nanti datanya keluar
             if (INSTANCE == null) {
                 synchronized(MovieRoomDatabase::class.java) {
-                    //builder() bikin instance nya dengan nama "movie_database"
-                    INSTANCE = databaseBuilder(
+                    INSTANCE = Room.databaseBuilder(
                         context.applicationContext,
                         MovieRoomDatabase::class.java, "movie_database"
                     )
+                        // Tambahkan migrasi dari versi 2 ke versi 1 di sini
+                        .addMigrations(MIGRATION_2_1)
                         .build()
                 }
             }
             return INSTANCE
+        }
+
+        // Migrasi dari versi 2 ke versi 1
+        private val MIGRATION_2_1: Migration = object : Migration(2, 1) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                // Logika migrasi dari versi 2 ke versi 1
+                // Misalnya, alter table, tambahkan kolom, dll.
+            }
         }
     }
 }
